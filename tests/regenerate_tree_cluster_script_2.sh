6fc284e6ac592926b9c1af1779fae0aa40355fe9
willboulton <wboulton12@gmail.com>
2022-01-29 17:37:17 +0000

    separator is now &

--- tests/regenerate_tree_cluster_script_2.sh
+++ tests/regenerate_tree_cluster_script_2.sh
@@ -14,7 +14,7 @@ python /hps/software/users/goldman/will/phastSim/tests/regenerate_tree_cluster.p
     --randomSeed 199 \
     --nLeaves 128 \
     --mutationsPerBranch 100 \
-    --raxmlModelString +-m+GTRCAT+-F+-c+3+--JC69+-f+B+-t+RAxML_result.rax_0 \
+    --raxmlModelString &--model&JC+R3 \
     --rootGenomeFrequencies 0.25+0.25+0.25+0.25 \
     --phastSimOptions +--categoryRates+0.1+1.0+10.0+--categoryProbs+0.25+0.5+0.25+--mutationRates+JC69 \
     --RAXMLNG True \
